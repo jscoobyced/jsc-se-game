06c1d3ab8546f116dbbbf045753169b2a6fdf064
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-09-27 23:58:46 +0700

    Add clouds with animation

--- code/src/scenes/JscSeGameIntro.ts
+++ code/src/scenes/JscSeGameIntro.ts
@@ -5,6 +5,9 @@ import general from '../config/general.json'
 export default class JscSeGameIntro extends Phaser.Scene {
   private player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody =
     null as unknown as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
+  private clouds: Phaser.GameObjects.Group = null as unknown as Phaser.GameObjects.Group
+  private lastMove = 'turn'
+  private isJumping = false
 
   preload = (): void => {
     this.load.image(assets.logo.key, assets.logo.value)
@@ -13,11 +16,14 @@ export default class JscSeGameIntro extends Phaser.Scene {
       frameWidth: assets.dude.frameWidth,
       frameHeight: assets.dude.frameHeight,
     })
+    this.load.spritesheet(assets.cloud.key, assets.cloud.value, {
+      frameWidth: assets.cloud.frameWidth,
+      frameHeight: assets.cloud.frameHeight,
+    })
   }
 
   create = (): void => {
     const logo = this.add.image(general.width / 2, 70, assets.logo.key)
-
     this.tweens.add({
       targets: logo,
       y: 100,
@@ -37,8 +43,23 @@ export default class JscSeGameIntro extends Phaser.Scene {
       general.height - assets.platform.height - assets.dude.frameHeight / 2,
       assets.dude.key,
     )
-    this.player.setBounce(0.2)
+    this.player.setBounce(0)
     this.player.setCollideWorldBounds(true)
+
+    const cloudChildren = []
+    for (let i = 0; i < 10; i++) {
+      cloudChildren.push(
+        this.add
+          .sprite(
+            Phaser.Math.Between(assets.cloud.frameWidth / 2, general.width),
+            Phaser.Math.Between(150, 350),
+            assets.cloud.key,
+          )
+          .setScale(Phaser.Math.FloatBetween(0.5, 1)),
+      )
+    }
+    this.clouds = this.add.group(cloudChildren)
+
     this.anims.create({
       key: 'left',
       frames: this.anims.generateFrameNumbers(assets.dude.key, { start: 0, end: 3 }),
@@ -59,27 +80,62 @@ export default class JscSeGameIntro extends Phaser.Scene {
       repeat: -1,
     })
 
+    this.anims.create({
+      key: 'stayLeft',
+      frames: [{ key: assets.dude.key, frame: 3 }],
+      frameRate: 20,
+    })
+
+    this.anims.create({
+      key: 'stayRight',
+      frames: [{ key: assets.dude.key, frame: 6 }],
+      frameRate: 20,
+    })
+
+    this.anims.create({
+      key: 'cloud',
+      frames: this.anims.generateFrameNumbers(assets.cloud.key, { start: 0, end: 2 }),
+      frameRate: 5,
+    })
+
     this.physics.add.collider(this.player, platforms)
   }
 
   update = (): void => {
     const cursors = this.input.keyboard.createCursorKeys()
+    let moving = 'turn'
     if (cursors.left.isDown) {
-      this.player.setVelocityX(-160)
-
-      this.player.anims.play('left', true)
+      this.player.setVelocityX(-100)
+      moving = 'left'
+      this.lastMove = 'stayLeft'
     } else if (cursors.right.isDown) {
-      this.player.setVelocityX(160)
-
-      this.player.anims.play('right', true)
+      this.player.setVelocityX(100)
+      moving = 'right'
+      this.lastMove = 'stayRight'
+    } else if (cursors.down.isDown) {
+      moving = 'turn'
+      this.lastMove = 'turn'
     } else {
-      this.player.setVelocityX(0)
-
-      this.player.anims.play('turn')
+      moving = this.lastMove
+      if (!this.isJumping || this.player.body.touching.down) {
+        this.player.setVelocityX(0)
+      }
     }
 
     if (cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330)
+      this.isJumping = true
+      this.player.setVelocityY(-200)
     }
+    this.player.anims.play(moving, moving != 'turn')
+    this.clouds.children.iterate((child, index) => {
+      const cloud = child as Phaser.GameObjects.Sprite
+      cloud.anims.play('cloud', true)
+      if (cloud.x - cloud.width / 2 > general.width) {
+        cloud.setX(-cloud.width / 2)
+        cloud.setY(Phaser.Math.Between(150, 350))
+      } else {
+        cloud.setX(cloud.x + 0.2 + index / 10)
+      }
+    })
   }
 }
