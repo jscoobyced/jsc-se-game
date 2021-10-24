966d2b47ea350c2b452f99ccf296246a30112132
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-10-24 19:38:22 +0700

    Rename and add main theme.

--- code/src/index.ts
+++ code/src/index.ts
@@ -1,9 +1,9 @@
 import Phaser from 'phaser'
 import config from './config'
-import JscSeGameIntro from './scenes/JscSeGameIntro'
+import JscMaGameIntro from './scenes/JscMaGameIntro'
 
 new Phaser.Game(
   Object.assign(config, {
-    scene: [JscSeGameIntro],
+    scene: [JscMaGameIntro],
   }),
 )
