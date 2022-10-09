84916eb6cdc8ebddcd356a1fb48dd9b4bd48a8de
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2022-10-09 21:50:27 +0700

    Add fullscreen on/off..

--- code/src/scenes/JscDefaultScene.ts
+++ code/src/scenes/JscDefaultScene.ts
@@ -15,12 +15,20 @@ export default class JscDefaultScene extends Phaser.Scene {
   defaultPreload = (): void => {
     this.load.image(`${this.level.key}-tiles`, `${general.baseUrls.images}/${this.level.tile}-map.png`)
     this.load.tilemapTiledJSON(`${this.level.key}-map`, `${general.baseUrls.json}/${this.level.tile}-map.json`)
+    this.load.spritesheet('fullscreen', 'assets/images/fullscreen.png', { frameWidth: 64, frameHeight: 64 })
   }
 
   createMap = () => {
     this.map = this.make.tilemap({ key: `${this.level.key}-map`, tileWidth: 64, tileHeight: 64 })
     this.tileset = this.map.addTilesetImage(this.level.main, `${this.level.key}-tiles`)
     this.map.createLayer(this.level.ground, this.tileset, 0, 0)
+    const fsButton = this.add
+      .image(general.width - 16, 16, 'fullscreen', 0)
+      .setOrigin(1, 0)
+      .setInteractive()
+    fsButton.on('pointerup', () => {
+      this.toggleFullScreen(fsButton)
+    })
   }
 
   createLayers = (player?: Phaser.Physics.Arcade.Sprite): void => {
@@ -33,5 +41,15 @@ export default class JscDefaultScene extends Phaser.Scene {
         this.physics.add.collider(player, current)
       }
     })
+  }
+
+  private toggleFullScreen = (fsButton: Phaser.GameObjects.Image) => {
+    if (this.scale.isFullscreen) {
+      fsButton.setFrame(0)
+      this.scale.stopFullscreen()
+    } else {
+      fsButton.setFrame(1)
+      this.scale.startFullscreen()
+    }
   }
 }
