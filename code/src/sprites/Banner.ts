71d3b1c48bcae15e48b9d084288b62a50ab49499
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2022-10-26 15:03:43 +0700

    Update font.

--- code/src/sprites/Banner.ts
+++ code/src/sprites/Banner.ts
@@ -3,27 +3,25 @@ import general from '../config/general.json'
 
 export default class Banner {
   private BANNER_PAD = 5
-  private TEXT_PAD = 50
-  private FONT_SIZE = 60
+  private TEXT_PAD = 60
+  private FONT_SIZE = 64
   private image!: Phaser.Types.Physics.Arcade.ImageWithDynamicBody
-  private text!: Phaser.GameObjects.BitmapText
+  private text!: Phaser.GameObjects.Text
   private onPointerUp!: () => void
 
   preload = (scene: Phaser.Scene): void => {
     scene.load.image('banner', `${general.baseUrls.images}/banner.png`)
-    scene.load.bitmapFont('me-font', `${general.baseUrls.fonts}/me-font.png`, `${general.baseUrls.fonts}/me-font.xml`)
   }
 
   create = (scene: Phaser.Scene): void => {
     this.image = scene.physics.add.image(0, 0, 'banner')
     this.image.setX(this.BANNER_PAD + this.image.displayWidth / 2)
     this.image.setY(this.BANNER_PAD + this.image.displayHeight / 2)
-    this.text = scene.add.bitmapText(
+    this.text = scene.add.text(
       this.image.originX + this.TEXT_PAD,
       general['banner-x'] + this.image.originY + this.TEXT_PAD,
-      'me-font',
       '',
-      this.FONT_SIZE,
+      { fontFamily: 'MumuFont', fontSize: `${this.FONT_SIZE}px`, color: 'brown' },
     )
   }
 
@@ -54,7 +52,6 @@ export default class Banner {
   }
 
   private executeCallback = () => {
-    console.log('Banner.executeCallback')
     if (this.onPointerUp) {
       this.onPointerUp()
     }
